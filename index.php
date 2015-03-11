d63df2941721e327698cd1f3eadc520a02e1583e
mpiranej <medin123@live.com>
2015-03-11 22:04:16 +0100

    punime te tjera

--- index.php
+++ index.php
@@ -3,16 +3,6 @@
      if(isset($_COOKIE["lg"]))$perd=$_COOKIE["lg"]; // Kontrollojme nqs ndonje perdorues eshte i loguar
 	 else $perd=-1;
 	 
-	 if(isset($_GET["emri"]))$kerk=$_GET["emri"];  // kontrollojme nqs eshte duke u kerkuar ndonje student me emer
-	 else $kerk=-1;
-	 
-	 if(isset($_GET["student"]))$stud=$_GET["student"];  // kontrollojme nqs eshte duke u kerkuar ndonje student me id
-	 else $stud=-1;
-	 
 	 if(isset($_GET["login"]))shfaq_login();
-	 else if(($kerk==-1)&&($stud==-1))shfaq_kerkim();
-	 else if($stud!=-1){
-	 	
-	 }
-
+	 else shfaq_kerkim();
 ?>
--- php/func.php
+++ php/func.php
@@ -6,7 +6,7 @@
 	function lidhu(){return new PDO('mysql:host=localhost;dbname=unishk', 'root', '');}
     function exec_query($query,$lidhja){$res=$lidhja->prepare($query);$res->execute();return $res->FetchAll();} // Funksion qe ekzekuton dhe kthen rezultatin ne nje assoc array ku indexet jane emrat e kolonave ne mysql psh . arr[nr_rreshti]['emer_kolone']
     function shfaq_kerkim(){
-    	shfaq_koken_e_faqes();
+    	shfaq_koken_e_faqes("Kerko","");
 	?>
 	<div class="content" >
 	<div id="divqendror" > 
@@ -89,14 +89,14 @@
 	<?php
 	shfaq_footer();
     }// funksion i cili do te shfaqe menune sapo te hapet faqja ku perdoruesi do te kete mundesi te logohet ose te kerkoje nje student
-    function shfaq_koken_e_faqes(){
+    function shfaq_koken_e_faqes($kat,$skripte){// kat do ta perdorim si "title" i html dhe $skripte kur dona me perdor skripte shtese si ne rasin e profili.php ku i shtojme nje css
     	?>
     	      <html>
                    <head>
-                       <title>Studens Site</title>
+                       <title><?php echo $kat.' | student site';// printojme titullin e faqes ?></title>              
                        <link href='css/stile_kryesore.css' rel='stylesheet' type='text/css'>
                        <script src="js/jquerylib.js"></script>
-                    
+                       <?php echo $skripte; // printojme skriptet shtese ?>
                     <script src="js/skriptiJquery.js" type="text/javascript" ></script>   
     
                    </head>
@@ -110,7 +110,7 @@ students site
 
 
 <ul>
-<li><a id="kycu"href="index.php?login=true&pageid=kycu">Kycuni</a></li><li>
+<li><a id="kycu"href="index.php?login=true">Kycuni</a></li><li>
      <a id="informacion"href="index.php?pageid=informacion">Informacion</a>
        <ul>
 	   <li><a id="rrethnesh" href="#">Rreth nesh</a></li>
@@ -149,10 +149,9 @@ students site
 </div></body></html><?php // kthen footerin ne menyre qe te printohet
     }
     function shfaq_login(){
-    	
-    	shfaq_koken_e_faqes();
-		?><div class='content' ><div class='logindivcontainer'><div class='logindiv'><form action='funksioni login' method='post'>
-<input type='text' name='username' id='username' placeholder='Emri i perdoruesit '></br><input type='password' name='password' id='password' placeholder='Fjalkalimi'><br>
+    	shfaq_koken_e_faqes("Login","");
+		?><div class='content' ><div class='logindivcontainer'><div class='logindiv'><form action='login.php' method='post'>
+<input type='text' name='emri' id='username' placeholder='Emri i perdoruesit '></br><input type='password' name='pas' id='password' placeholder='Fjalkalimi'><br>
 <input type='button' name='forgotpassbtn' id='forgotpassbtn' value='Keni harruar fjalkalimin?'>
 <input type='submit' name='loginbtn' id='loginbtn' value='Hyr'>
 </form>
