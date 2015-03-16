c2073a6caf5b4a5939cba73811b2b3f234a48aab
Bamirs <bamirsi_94@hotmail.com>
2015-03-17 00:32:20 +0100

    kam shtu te dhanat te divi i rezulatatit email, dega,adresa

--- js/skripte.js
+++ js/skripte.js
@@ -10,14 +10,19 @@ function kerko(){
    
    
      $(".topfilterdiv").slideDown('normal');
-	document.getElementById("divi_i_rezultatit").style.display="inline";
+	document.getElementById("divi_i_rezultatit").style.display="block";
 	
     json=JSON.parse(httpGet("kerko.php?kerko="+kerko));
     i=0;
     document.getElementById("divi_i_rezultatit").innerHTML="";//fshijem divin
+    document.getElementById("divi_i_rezultatit").style.display="inline-block";
+   $("#divi_i_rezultatit").css("width","100%");
     if(json[0][0]!=undefined)//kontrollojme nese kemi ndonje rezultat
 	while(json[i]!=undefined){ // printojme rezultatet 
-	document.getElementById("divi_i_rezultatit").innerHTML+="<div class='rezdivleft' ><a href='student.php?student="+json[i]["stud_id"]+"'>"+json[i]["emri"]+" "+json[i]["mbiemri"]+"</a> <p>ktu do fusim te dhena te tjera mrapa</p> </div>";
+         var temphtml =$("#divi_i_rezultatit").html();
+        $("#divi_i_rezultatit").html(temphtml+"<a href='student.php?student="+json[i]["stud_id"]+"'> <div class='rezdivleft' ><div id='div_rez_foto'><img src='img/def_profile_pic.jpg' width='70'></div><h4 id='div_rez_emer'>"+json[i]["emri"]+" "+json[i]["mbiemri"]+"</h4><h5 id='div_rez_dega'>Email:&nbsp;<i>"+json[i]["email"]+"</i></h5> <h5 id='div_rez_dega'>Dega:&nbsp;<i>"+json[i]["dega"]+"</i></h5> <h5 id='div_rez_adresa'>Adresa:&nbsp;<i>"+json[i]["adresa"]+"</i></h5></div></a>");
+        
+	
     i++;
     }
     else document.getElementById("divi_i_rezultatit").innerHTML="nuk u gjet gje"+json[0];
