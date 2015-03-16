0a7a980169315b203f3e167db470374bca337805
Bamirs <bamirsi_94@hotmail.com>
2015-03-17 00:30:07 +0100

    kam shtu divin e majt per me shkru informacionin per studentat si me ba regjistrimin

--- hyr.php
+++ hyr.php
@@ -1,11 +1,16 @@
 <?php
      include 'php/func.php';
-	 $kat="Login !";
+	 $kat="Login ";
 	 $perd=-1;
 	 shfaq_koken_e_faqes($kat,"");
 ?>
 <div class='content' id="login_id">
-<div class='logindivcontainer'><div class='logindiv'><form action='login.php' method='post'>
+   <div class='logindivcontainerleft'>
+       <div class='logindivinfo'>
+           informacion per rregjistrimin e studentave 
+       </div>
+    </div>
+<div class='logindivcontainerright'><div class='logindiv'><form action='login.php' method='post'>
 <input type='text' name='emri' id='username' placeholder='Emri i perdoruesit '></br><input type='password' name='pas' id='password' placeholder='Fjalkalimi'><br>
 <input type='button' name='forgotpassbtn' id='forgotpassbtn' value='Keni harruar fjalkalimin?'>
 <input type='submit' name='loginbtn' id='loginbtn' value='Hyr'>
