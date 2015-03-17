b971d742d9b3514f0556460d5068ca448133718e
Bamirs <bamirsi_94@hotmail.com>
2015-03-17 19:17:17 +0100

    skam ndryshu gja

--- hyr.php
+++ hyr.php
@@ -1,7 +1,5 @@
 <?php
      include 'php/func.php';
-<<<<<<< HEAD
-
 
 	 $kat="Login ";
 
@@ -10,26 +8,16 @@
 ?>
 <div class='content' id="login_id">
 
-=======
-	 $kat="Login ";
-	 $perd=-1;
-	 shfaq_koken_e_faqes($kat,"");
-?>
-    	<div class='content' id="login_id">
->>>>>>> f8a5d3d3c8f16544588021255e3b67bd3b2bdae5
    <div class='logindivcontainerleft'>
        <div class='logindivinfo'>
            informacion per rregjistrimin e studentave 
        </div>
     </div>
 <div class='logindivcontainerright'><div class='logindiv'><form action='login.php' method='post'>
-<<<<<<< HEAD
+
 
 <input type='text' name='email' id='username' placeholder='Email'></br><input type='password' name='pas' id='password' placeholder='Fjalkalimi'><br>
 
-=======
-<input type='text' name='emri' id='username' placeholder='Emri i perdoruesit '></br><input type='password' name='pas' id='password' placeholder='Fjalkalimi'><br>
->>>>>>> f8a5d3d3c8f16544588021255e3b67bd3b2bdae5
 <input type='button' name='forgotpassbtn' id='forgotpassbtn' value='Keni harruar fjalkalimin?'>
 <input type='submit' name='loginbtn' id='loginbtn' value='Hyr'>
 </form>
