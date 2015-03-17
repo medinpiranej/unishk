f8a5d3d3c8f16544588021255e3b67bd3b2bdae5
mpiranej <medin123@live.com>
2015-03-17 16:22:22 +0100

    "rregulluar difektet e merget nga medini"
    Please enter the commit message for your changes. Lines starting

--- hyr.php
+++ hyr.php
@@ -4,17 +4,13 @@
 	 $perd=-1;
 	 shfaq_koken_e_faqes($kat,"");
 ?>
-<div class='content' id="login_id">
-<<<<<<< HEAD
+    	<div class='content' id="login_id">
    <div class='logindivcontainerleft'>
        <div class='logindivinfo'>
            informacion per rregjistrimin e studentave 
        </div>
     </div>
 <div class='logindivcontainerright'><div class='logindiv'><form action='login.php' method='post'>
-
-<div class='logindivcontainer'><div class='logindiv'><form action='login.php' method='post'>
-
 <input type='text' name='emri' id='username' placeholder='Emri i perdoruesit '></br><input type='password' name='pas' id='password' placeholder='Fjalkalimi'><br>
 <input type='button' name='forgotpassbtn' id='forgotpassbtn' value='Keni harruar fjalkalimin?'>
 <input type='submit' name='loginbtn' id='loginbtn' value='Hyr'>
@@ -22,6 +18,7 @@
 </div>
 </div>
 </div>
+
 <?php
    shfaq_footer("");
 ?>
