defdbaea53859d0bc2bf7ba91a309327200f95bf
medinpiranej <medin123@live.com>
2015-03-21 21:17:07 +0100

    rruges per tu pershtatur me skemen e re te databazes

--- ndrysho_student.php
+++ ndrysho_student.php
@@ -21,12 +21,12 @@ if( $tipi_fotos != "jpg" &&  $tipi_fotos != "png" && $tipi_fotos != "jpeg"&&  $t
 if ($foto_e_vlefshme != 0) {
     mkdir("perd".$perd[0]["stud_id"]);
     mkdir("perd".$perd[0]["stud_id"]."/foto");
-    if($perd[0]["foto"]!="img/def_profile_pic.jpg")unlink($perd[0]["foto"]);// fshihet fotoja e vjeter nqs nuk eshte fotoja default
+    if($perd[0]["s_foto"]!="img/def_profile_pic.jpg")unlink($perd[0]["s_foto"]);// fshihet fotoja e vjeter nqs nuk eshte fotoja default
     
     move_uploaded_file($_FILES["f_profili"]["tmp_name"], $foto_dir);
 }else $foto_dir=$perd[0]["foto"];
 
-    exec_query("update student set foto='{$foto_dir}' where `stud_id`=".$perd[0]["stud_id"], $lidhja);
+    exec_query("update student set s_foto='{$foto_dir}' where `stud_id`=".$perd[0]["stud_id"], $lidhja);
     $_SESSION["perdorues"]=exec_query("Select * from student where stud_id=".$perd[0]["stud_id"], $lidhja);
     header("location: student.php?student=".$perd[0]["stud_id"]);
 ?>
