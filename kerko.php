7f8731817f79a39b3c4de8822e98b526f8f87123
medinpiranej <medin123@live.com>
2015-03-21 18:21:28 +0100

    permirsuar kerkimi

--- kerko.php
+++ kerko.php
@@ -9,7 +9,8 @@
      
      $gjatesi=sizeof($fjale);
      switch ($gjatesi) {
-         case 1:$persona=exec_query("select * from student where (emri like '%{$fjale[0]}%' or strcmp(soundex(emri), soundex('{$fjale[0]}')) = 0 or mbiemri like '%{$fjale[0]}%' or strcmp(soundex(mbiemri), soundex('{$fjale[0]}')) = 0)", $lidhja);
+         case 1:if(stripos($kerkim,"@")==-1) $persona=exec_query("select * from student where (emri like '%{$fjale[0]}%' or strcmp(soundex(emri), soundex('{$fjale[0]}')) = 0 or mbiemri like '%{$fjale[0]}%' or strcmp(soundex(mbiemri), soundex('{$fjale[0]}')) = 0)", $lidhja);
+	            else $persona=exec_query("select * from student where (email like '%{$kerkim}%' or strcmp(soundex(email), soundex('{$kerkim}')) = 0)",$lidhja);
              break;
          case 2:$persona=exec_query("select * from student where (emri like '%{$fjale[0]}%' and mbiemri like '%{$fjale[1]}%') or (emri like '%{$fjale[1]}%' and mbiemri like '%{$fjale[0]}%') or ((strcmp(soundex(emri), soundex('{$fjale[0]}')) = 0) and (strcmp(soundex(mbiemri), soundex('{$fjale[1]}')) = 0)) or ((strcmp(soundex(emri), soundex('{$fjale[1]}')) = 0) and (strcmp(soundex(mbiemri), soundex('{$fjale[0]}')) = 0))", $lidhja);
              break;
@@ -17,6 +18,5 @@
 	 
      if(!empty($persona))echo json_encode($persona);
 	 else echo json_encode( array(-1) );
-	 
 	 $lidhja=null;
      ?>
